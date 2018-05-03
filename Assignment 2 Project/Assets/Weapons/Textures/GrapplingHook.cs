d6c4356087906d9092a35a0d19a66e85244354e8
maxdraz <38158634+maxdraz@users.noreply.github.com>
2018-05-03 22:16:32 +0100

    Started implementing grappling and shooting
    Imported some standard assets

---
+++ Assignment 2 Project/Assets/Weapons/Textures/GrapplingHook.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrapplingHook : MonoBehaviour {
+
+	public GameObject hook;
+	public GameObject hookHolder;
+
+	public float hookTravelSpeed;
+	public float playerTravelSpeed;
+
+	public static bool fired;
+	public static bool hooked;
+
+	public float maxDistance;
+	private float currentDistance;
+
+	void Update(){
+
+		// firing the hook
+	if (Input.GetMouseButtonDown (0) && fired == false)
+			fired = true;
+
+		if (fired == true) {
+			hook.transform.Translate (Vector3.forward * Time.deltaTime * playerTravelSpeed);
+			currentDistance = Vector3.Distance (transform.position, hook.transform.position);
+
+			if (currentDistance >= maxDistance)
+				ReturnHook ();
+		}
+
+	}
+
+	void ReturnHook(){
+		hook.transform.position = hookHolder.transform.position;
+		fired = false;
+		hooked = false;
+	}
+}
